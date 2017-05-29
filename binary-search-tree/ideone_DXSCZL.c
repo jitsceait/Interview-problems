f488d372bff2e10ffce11b34347c0ca896b7ab8a
Sangar Jitendra <jitsceait@gmail.com>
2017-05-29 20:38:04 +0200

    WAP to find height of binary tree

--- binary-search-tree/ideone_DXSCZL.c
+++ binary-search-tree/ideone_DXSCZL.c
@@ -3,57 +3,60 @@
 #include<math.h>
  
 struct node{
-	int value;
-	struct node *left;
-	struct node *right;
+    int value;
+    struct node *left;
+    struct node *right;
 };
 typedef struct node Node;
 
 #define MAX(a,b)  (a < b ? b : a)
 
 Node *createNode(int value){
-	Node * newNode =  (Node *)malloc(sizeof(Node));
-	newNode->value = value;
-	newNode->right= NULL;
-	newNode->left = NULL;
+    Node * newNode =  (Node *)malloc(sizeof(Node));
 	
-	return newNode;
+    newNode->value = value;
+    newNode->right= NULL;
+    newNode->left = NULL;
+	
+    return newNode;
 }
+
 Node *addNode(Node *node, int value){
-	if(!node){
+    if(!node)
 		return createNode(value);
-	}
-	if (node->value > value)
-		node->left = addNode(node->left, value);
-	else
-		node->right = addNode(node->right, value);
+        
+    if (node->value > value)
+        node->left = addNode(node->left, value);
+    else
+        node->right = addNode(node->right, value);
 		
-	return node;
+    return node;
 }
 
 int height( Node *root){
-	if( !root ) 
-		return 0;
+    if( !root ) return 0;
+    
+    int lheight = height( root->left);
+    int rheight = height( root->right);
 	
-	int lheight = height( root->left);
-	int rheight = height( root->right);
-	
-	return 1 + MAX (lheight, rheight );
+    return 1 + MAX (lheight, rheight );
 }
 
 /* Driver program for the function written above */
 int main(){
-	Node *root = NULL;
-	//Creating a binary tree
-	root = addNode(root, 30);
-	root = addNode(root, 20);
-	root = addNode(root, 15);
-	root = addNode(root, 25);
-	root = addNode(root, 40);
-	root = addNode(root, 38);
-	root = addNode(root, 39);
-	root = addNode(root, 45);
-	printf( "Height of tree is : %d", height( root));
+    Node *root = NULL;
 	
-	return 0;
+    //Creating a binary tree
+    root = addNode(root, 30);
+    root = addNode(root, 20);
+    root = addNode(root, 15);
+    root = addNode(root, 25);
+    root = addNode(root, 40);
+    root = addNode(root, 38);
+    root = addNode(root, 39);
+    root = addNode(root, 45);
+    
+    printf( "Height of tree is : %d", height( root));
+    
+    return 0;
 }
