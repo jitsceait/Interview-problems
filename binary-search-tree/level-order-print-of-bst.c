1d97dbd01183501aa1fbc5ce92b876dd5bcfa165
Sangar Jitendra <jitsceait@gmail.com>
2017-05-29 21:11:58 +0200

    WAP to level order traversal of BST

--- binary-search-tree/level-order-print-of-bst.c
+++ binary-search-tree/level-order-print-of-bst.c
@@ -18,95 +18,93 @@ typedef struct queue Queue;
 
 
 void enqueue(Queue **queue, Node *node){
-	Queue *q  = NULL;
-	Queue *head  = *queue;
+    Queue *q  = NULL;
+    Queue *head  = *queue;
 	
-	if(!(*queue)){
-		q =(Queue *)malloc(sizeof(Queue));
-		q->element = node;
-		q->next = NULL;
-		*queue = q;
-	}
-	else{
-		q = *queue;
-		while(q->next)
-			q = q->next;
+    if(!(*queue)){
+	q =(Queue *)malloc(sizeof(Queue));
+	q->element = node;
+	q->next = NULL;
+	*queue = q;
+    }
+    else{
+	q = *queue;
+	while(q->next)
+	    q = q->next;
 		
-		Queue *temp =(Queue *)malloc(sizeof(Queue));
-		q->next = temp;
-		temp->element = node;
-		temp->next = NULL;
-		*queue = head;
-	}
+	Queue *temp =(Queue *)malloc(sizeof(Queue));
+	q->next = temp;
+	temp->element = node;
+	temp->next = NULL;
+	*queue = head;
+    }
 }
 
 int isEmpty( Queue *q){
-	if(!q) return 1;
+     if(!q) return 1;
 	
-	return 0;
+     return 0;
 }
+
 Node *front(Queue *queue){
-	if(queue)
-		return queue->element;
+    if(queue)
+	return queue->element;
 }
 
 void dequeue(Queue **queue){
-	Queue *q = *queue;
+    Queue *q = *queue;
 	
-	Queue *nodeToDelete = q;
-	q = nodeToDelete->next;
-	nodeToDelete = NULL;
-	free(nodeToDelete);
+    Queue *nodeToDelete = q;
+    q = nodeToDelete->next;
+    nodeToDelete = NULL;
+    free(nodeToDelete);
 	
-	*queue = q;
+    *queue = q;
 }
 
 Node * createNode(int value){
     Node * newNode =  (Node *)malloc(sizeof(Node));
+    
     newNode->value = value;
     newNode->right = NULL;
     newNode->left = NULL;
     
     return newNode;
 }
+
 Node * addNode(Node *node, int value){
-    if(!node){
+    if(!node)
         return createNode(value);
-    }
     else{
-        if (node->value > value){
+        if (node->value > value)
             node->left = addNode(node->left, value);
-        }
-        else{
+        else
             node->right = addNode(node->right, value);
-        }
     }
     return node;
 }
 
 void printTreeLevel(Node * node){
-
- Queue *q =  NULL;
- enqueue(&q, node);
+    Queue *q =  NULL;
+    enqueue(&q, node);
  
- while(!isEmpty(q)){
- 	Node *currentNode = front(q);
- 	
- 	printf("%d ", currentNode->value);
-    
-    if(currentNode->left)
-     	enqueue(&q, currentNode->left);
-    if(currentNode->right)
-    	enqueue(&q, currentNode->right);
+     while(!isEmpty(q)){
+         Node *currentNode = front(q);
+         printf("%d ", currentNode->value);
+         if(currentNode->left)
+             enqueue(&q, currentNode->left);
+         if(currentNode->right)
+    	     enqueue(&q, currentNode->right);
 	
-	dequeue(&q);
- }
+         dequeue(&q);
+     }
 }
+
 /* Driver program for the function written above */
 int main(){
     Node *root = NULL;
 	
-	//Creating a binary tree
+    //Creating a binary tree
     root = addNode(root,30);
     root = addNode(root,20);
     root = addNode(root,15);
