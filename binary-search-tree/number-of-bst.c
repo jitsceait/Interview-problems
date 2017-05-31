f5198cb16dc1ddbddf868f2b9ea7f7e00e9f346b
Sangar Jitendra <jitsceait@gmail.com>
2017-05-31 08:38:16 +0200

    WAP to find number of BST with N nodes

--- binary-search-tree/number-of-bst.c
+++ binary-search-tree/number-of-bst.c
@@ -23,6 +23,24 @@ int number_of_trees(int n){
     return sum;
 }
 
+int number_of_trees_dp(int n)
+{
+	int i, j;
+    // Table to store results of subproblems
+    int Trees[n+1];
+ 
+    // Initialize first two values in table
+    Trees[0] = Trees[1] = 1;
+ 
+    for (i=2; i<=n; i++)
+    {
+        Trees[i] = 0;
+        for (j=0; j<i; j++)
+            Trees[i] += Trees[j] * Trees[i-j-1];
+    }
+    return Trees[n];
+}
+
 int main(){
     printf("\n%d", number_of_trees(3));
     return 0;
