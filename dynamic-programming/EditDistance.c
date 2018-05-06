9fc02d7dd40689e97a21c6d6ca5797f0ed6b467d
Sangar Jitendra <jitsceait@gmail.com>
2018-05-06 06:47:59 +0200

    Update EditDistance.c

--- dynamic-programming/EditDistance.c
+++ dynamic-programming/EditDistance.c
@@ -21,7 +21,52 @@ int editDistance(char *s1, char *s2, int length1, int length2){
         		( 1+ editDistance(s1,s2,length1, length2-1))),
         		( isCharacterEqual + editDistance(s1,s2, length1-1,length2-1)) );
 }
-//Driver program
+
+int editDistance(char *s1, char *s2){
+	int n = strlen(s1);
+	int m = strlen(s2);
+ 
+    int minimumDistance = 0;
+    int currentMinimum  = 0;
+    int Table[n+1][m+1] ;
+    
+    memset(Table,0, sizeof(Table));
+    
+    //Intitialization
+    for(int i=0; i<=n; i++)
+        Table[i][0] =i;
+
+    for(int i=1; i<=m; i++)
+        Table[0][i] = i;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            //Case 3 : Possibility 1 :If X[i] == Y[j] 
+            if(s1[i-1] == s2[j-1]){
+                currentMinimum = Table[i-1][j-1];
+            }
+            //Case 3 : Possibility 2 :If X[i] != Y[j] 
+            else{
+                currentMinimum =  Table[i-1][j-1] + 1;
+            }
+            //Case 1 : Deletion of character from S1 
+            if(Table[i][j-1] > Table[i-1][j]){
+                minimumDistance = Table[i-1][j] + 1;
+            }
+            //Case 2 : Addition of character on S1 
+            else {
+                minimumDistance = Table[i][j-1] + 1;
+            }
+
+            if(currentMinimum < minimumDistance){
+            	minimumDistance = currentMinimum;
+            }
+            Table[i][j] = minimumDistance;
+        }
+    }
+    return Table[n-1][m-1];
+}
+
 int main(){
 
     char *s = "EXPONENTIAL";
