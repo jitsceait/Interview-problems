1f71653a179efcc674e9077d803baf4988b12628
Sangar Jitendra <jitsceait@gmail.com>
2018-05-06 06:52:52 +0200

    Update PossiblePath.c

--- dynamic-programming/PossiblePath.c
+++ dynamic-programming/PossiblePath.c
@@ -18,7 +18,17 @@ int PossiblePaths(int m,int n){
 	}
 	return Table[n];
 }
-
+ 
+int PossiblePaths(int i,int j, int m, int n){
+	if(i > m || j > n) return 0; 
+ 
+	if(i == m && j == n) return 1;
+ 
+	return PossiblePaths(i+1,j, m,n) 
+			+ PossiblePaths(i, j+1, m,n) 
+			+ PossiblePaths(i+1, j+1,m,n);
+}
+ 
 int PossiblePaths(int m,int n){
 	int Table[m+1][n+1];
 	int i,j;
