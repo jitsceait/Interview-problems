5d3b42058cd63fe1864888dea6267d1452dccc20
Sangar Jitendra <jitsceait@gmail.com>
2018-05-06 06:29:51 +0200

    Update longestCommonSubsequnce.c

--- dynamic-programming/longestCommonSubsequnce.c
+++ dynamic-programming/longestCommonSubsequnce.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+int longestCommonSubsequnceLength(char * A, char * B) {
+	int lenA = strlen(A);
+ 	int lenB = strlen(B);
+	
+	int TableA[lenA+1];
+	int TableB[lenB+1];
+	
+	for (int i = lenA; i >= 0; i--){
+		for (int j = lenB; j >= 0; j--){
+			if (A[i] == '\0' || B[j] == '\0'){
+				TableA[j] = 0;	
+			}
+			else if (A[i] == B[j]){
+				TableA[j] = 1 + TableB[j+1];	
+			} 
+			else{
+				TableA[j] = max(TableB[j], TableA[j+1]);	
+			} 
+	    }
+	    for(int k =0; k<=lenA; k++){
+	    	TableB[k] = TableA[k];
+	    }
+	}
+	return TableA[0];
+}
+
 int max(int a, int b){
 	return a>b ? a:b;
 }
