1b8bb3163c2d232bd811911c993cbd04b1387262
Sangar Jitendra <jitsceait@gmail.com>
2018-05-06 06:51:30 +0200

    Update MinimumCostPath.c

--- dynamic-programming/MinimumCostPath.c
+++ dynamic-programming/MinimumCostPath.c
@@ -19,16 +19,42 @@ int findMinimumCostPath(int Cost[3][3], int i, int j){
    return min(findMinimumCostPath(Cost,i-1, j), 
     		   findMinimumCostPath(Cost,i, j-1)) + Cost[i][j];
 }
+
+int findMinimumCostPath(int Cost[3][3], int M, int N){
+	
+	int MinCost[M][N]; //declare the minCost matrix
+
+    MinCost[0][0] = Cost[0][0];
+
+    // initialize first row of MinCost matrix
+    for (int i=1; i<N; i++){
+        MinCost[0][i] = MinCost[0][i-1] + Cost[0][i];
+    }
+
+    for (int i=1; i<M; i++){
+        MinCost[i][0] = MinCost[i-1][0] + Cost[i][0];
+    }
+    
+    for (int i=1;i<M; i++){
+    	for (int j=1; j<N; j++){
+           MinCost[i][j] = min(MinCost[i-1][j],MinCost[i][j-1]) + Cost[i][j];
+        }
+    }
+
+    return MinCost[M-1][N-1];
+    
+}
+
 int main()
 {
     int M,N; 
     
     M = N = 3; 
     int Cost[3][3] = {
-    					1,3,4,
-    					5,3,2,
-    					3,4,5
+    	1,3,4,
+    	5,3,2,
+    	3,4,5
     };
-	  printf("Minimum cost of path : %d" , findMinimumCostPath(Cost, M-1, N-1));
+    printf("Minimum cost of path : %d" , findMinimumCostPath(Cost, M-1, N-1));
     
 }
